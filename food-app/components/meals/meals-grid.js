d40792c5bd4af1a3523ea9739a2dd4f36c7956ce
Santosh Kumar <santoshku8287@gmail.com>
2025-05-12 22:33:36 +0530

    SQLite Database added for getMeals data

--- food-app/components/meals/meals-grid.js
+++ food-app/components/meals/meals-grid.js
@@ -2,7 +2,7 @@ import classes from './meal-grid.module.css'
 import MealItem from './meal-item';
 export default function MealsGrid({ meals }) {
   return (
-    <ul>
+    <ul className={classes.meals}>
       {meals.map((meal) => (
         <li key={meal.id}>
             <MealItem {...meal}/>
