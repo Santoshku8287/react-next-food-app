90d7d1b03f5dab29f8a0107aa06e59b9948f2790
Santosh Kumar <santoshku8287@gmail.com>
2025-05-12 22:19:45 +0530

    meals page created with grid

---
+++ food-app/components/meals/meals-grid.js
@@ -0,0 +1,13 @@
+import classes from './meal-grid.module.css'
+import MealItem from './meal-item';
+export default function MealsGrid({ meals }) {
+  return (
+    <ul>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+            <MealItem {...meal}/>
+        </li>
+      ))}
+    </ul>
+  );
+}
