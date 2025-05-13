c192404b3e336ed71d1320f29670937e2473e212
Santosh Kumar <santoshku8287@gmail.com>
2025-05-13 20:47:24 +0530

    meal detail page not found page added

--- food-app/app/meals/[mealSlug]/page.js
+++ food-app/app/meals/[mealSlug]/page.js
@@ -1,10 +1,15 @@
 import classes from "./page.module.css";
 import Image from "next/image";
 import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
 
 
 export default function MealDetailsPage({params}) {
   const meal = getMeal(params.mealSlug);
+  if(!meal){
+    //closest not found error page if no meal is available in db
+    notFound();
+  }
   meal.instructions = meal.instructions.replace(/\n/g, '<br/>')
   return (
     <>
